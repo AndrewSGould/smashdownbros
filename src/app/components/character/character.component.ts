4d1c798c5e33e3c287182cf7eba15254f530ef90
Andrew Scott Gould <agould743@gmail.com>
2022-03-01 00:03:26 -0500

    get rid of sample code, get the defaults GOING

---
+++ src/app/components/character/character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sdb-character',
+  templateUrl: './character.component.html',
+  styleUrls: ['./character.component.scss']
+})
+export class CharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
